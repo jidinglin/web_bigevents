1da93c6f6643dbc5262fa15fd0c4cca65dd43f39
jidinglin <2842449233@qq.com>
2023-02-25 15:51:13 +0800

    完成了主页功能的开发

--- js/baseAPI.js
+++ js/baseAPI.js
@@ -1,3 +1,9 @@
 $.ajaxPrefilter(function(options) {
     options.url += 'http://www.liulongbin.top:3007'
+
+    if(options.url.indexOf('/my') !== -1) {
+        options.headers = {
+            Authorization: localStorage.getItem('token') || ''
+        }
+    }
 })
---
+++ js/index.js
@@ -0,0 +1,59 @@
+$(function () {
+    getUserInfo()
+
+    $('#btnLogout').on('click', function () {
+        layer.confirm('确定退出登录?', { icon: 3, title: '提示' }, function (index) {
+            // 1.清空token
+            localStorage.removeItem('token')
+            // 2.跳转到登录页面
+            location.href = '/login.html'
+            layer.close(index);
+        });
+    })
+})
+
+// 获取用户信息
+function getUserInfo() {
+    $.ajax({
+        method: 'GET',
+        url: '/my/userinfo',
+        headers: {
+            Authorization: localStorage.getItem('token') || ''
+        },
+        success: function (res) {
+            if (res.status !== 0) {
+                return layui.layer.msg('获取用户信息失败')
+            }
+            // 渲染用户头像
+            renderAvatar(res.data)
+        },
+        complete: function(res) {
+            if(res.responseJSON.status === 1
+                 && res.responseJSON.message ==='身份认证失败') {
+                    localStorage.removeItem('token')
+                    location.href = '/login.html'
+            }
+        }
+
+    })
+}
+
+// 渲染用户头像
+function renderAvatar(user) {
+    // 文字
+    let name = user.nickname || user.username
+    $('.welcome').html('欢迎&nbsp;&nbsp;' + name)
+
+    // 头像
+    if (user.user_pic !== null) {
+        // 图片头像
+        $('.layui-nav-img').attr('src', user.user_pic).show()
+        $('.text-avatar').hide()
+    } else {
+        // 文字头像
+        let first = name[0].toUpperCase()
+        $('.text-avatar').html(first).show()
+        $('.layui-nav-img').hide()
+    }
+
+}
